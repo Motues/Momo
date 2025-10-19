f1acd86369607da52c1cf64f17448c1a7c2cdebc
Motues <2901987286@qq.com>
2025-10-19 13:11:13 +0800

    fix: 修复blog图片位置错误

--- src/utils/url-utils.ts
+++ src/utils/url-utils.ts
@@ -1,5 +1,18 @@
 import { i18n } from "astro:config/client";
 
+function joinUrl(...parts: string[]): string {
+	const joined = parts.join("/");
+	return joined.replace(/\/+/g, "/");
+}
+/**
+ * 构建完整的URL路径
+ * @param path - 需要拼接的路径片段
+ * @returns 返回拼接后的完整URL路径
+ */
+export function baseUrl(path: string) {
+	return joinUrl("", import.meta.env.BASE_URL, path);
+}
+
 /**
  * 将相对于content/blog目录的路径转换为相对于src目录的路径
  * @param contentPath 相对于content/blog目录的路径
@@ -18,24 +31,8 @@ export function blogCoverUrl(contentPath: string, blogName: string): string {
     // 移除可能的前导斜杠
     const normalizedPath = contentPath.startsWith('/') ? contentPath.slice(1) : contentPath;
     
-    // 提取 blogName 中的文件夹名称（例如从 "intro/index.md" 提取 "intro"）
-    const blogFolder = blogName.split('/')[0];
-    
     // 构造相对于src目录的路径，包含博客名称文件夹
-    return `content/blog/${blogFolder}/${normalizedPath}`;
-}
-
-function joinUrl(...parts: string[]): string {
-	const joined = parts.join("/");
-	return joined.replace(/\/+/g, "/");
-}
-/**
- * 构建完整的URL路径
- * @param path - 需要拼接的路径片段
- * @returns 返回拼接后的完整URL路径
- */
-export function baseUrl(path: string) {
-	return joinUrl("", import.meta.env.BASE_URL, path);
+    return joinUrl("content/blog/", blogName, normalizedPath)
 }
 
 /**
